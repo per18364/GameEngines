8e614c7f27910b064f1c5fab52c2fe9ef07c4c11
Jorge Pérez <77463563+per18364@users.noreply.github.com>
2022-09-07 05:15:22 -0600

    Edicion de Sprites

--- src/Game/game.cpp
+++ src/Game/game.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "game.h"
+#include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include "stexture.cpp"
 
 Game::Game()
 {
@@ -19,26 +21,54 @@ void Game::init(const char *title, int width, int height)
     isRunning = true;
 }
 
+STexture *tex;
+
+Uint32 fragment(Uint32 currentColor, STexture *skin)
+{
+    Uint8 r = (currentColor >> 16) & 0xFF;
+    Uint8 g = (currentColor >> 8) & 0xFF;
+    Uint8 b = currentColor & 0xFF;
+
+    if (r == 0 && g == 0)
+    {
+        return currentColor;
+    }
+
+    std::cout << "r: " << (int)r << " g: " << (int)g << std::endl;
+
+    Uint32 skinnedColor = skin->getPixel(r, g);
+    std::cout << "skinnedColor: " << (int)skinnedColor << std::endl;
+
+    return skinnedColor;
+}
+
 void Game::setup()
 {
     std::cout << "Game Setting Up..." << std::endl;
 
     SDL_GetRendererOutputSize(renderer, &window_w, &window_h);
 
-    player1.x = 5;
-    player1.y = 200;
-    player1.w = 20;
-    player1.h = window_h / 4;
+    // player1.x = 5;
+    // player1.y = 200;
+    // player1.w = 20;
+    // player1.h = window_h / 4;
 
-    player2.y = 200;
-    player2.w = 20;
-    player2.x = (window_w - 5) - player2.w;
-    player2.h = window_h / 4;
+    // player2.y = 200;
+    // player2.w = 20;
+    // player2.x = (window_w - 5) - player2.w;
+    // player2.h = window_h / 4;
 
     ball.x = 350;
     ball.y = 350;
-    ball.w = 25;
-    ball.h = 25;
+    ball.w = 200;
+    ball.h = 200;
+
+    STexture *skin = new STexture(renderer, window);
+    skin->load("./src/Colores.png");
+
+    tex = new STexture(renderer, window);
+    tex->load("./src/Rei1.0.png");
+    tex->executeShader(fragment, skin);
 }
 
 void Game::handleEvents()
@@ -52,28 +82,28 @@ void Game::handleEvents()
         {
             isRunning = false;
         }
-        else if (event.type == SDL_KEYDOWN)
-        {
-            std::cout << "KEY PRESSED" << std::endl;
+        // else if (event.type == SDL_KEYDOWN)
+        // {
+        //     std::cout << "KEY PRESSED" << std::endl;
 
-            if (event.key.keysym.sym == SDLK_UP)
-            {
-                player2.y -= 10;
-            }
-            else if (event.key.keysym.sym == SDLK_DOWN)
-            {
-                player2.y += 10;
-            }
+        //     if (event.key.keysym.sym == SDLK_UP)
+        //     {
+        //         player2.y -= 10;
+        //     }
+        //     else if (event.key.keysym.sym == SDLK_DOWN)
+        //     {
+        //         player2.y += 10;
+        //     }
 
-            if (event.key.keysym.sym == SDLK_s)
-            {
-                player1.y += 10;
-            }
-            else if (event.key.keysym.sym == SDLK_w)
-            {
-                player1.y -= 10;
-            }
-        }
+        //     if (event.key.keysym.sym == SDLK_s)
+        //     {
+        //         player1.y += 10;
+        //     }
+        //     else if (event.key.keysym.sym == SDLK_w)
+        //     {
+        //         player1.y -= 10;
+        //     }
+        // }
     }
 }
 
@@ -81,64 +111,64 @@ void Game::update()
 {
     std::cout << "Game Updating..." << std::endl;
 
-    if (player2.y >= window_w - player2.h)
-    {
-        player2.y = window_w - player2.h;
-    }
+    // if (player2.y >= window_w - player2.h)
+    // {
+    //     player2.y = window_w - player2.h;
+    // }
 
-    if (player2.y <= 0)
-    {
-        player2.y = 0;
-    }
+    // if (player2.y <= 0)
+    // {
+    //     player2.y = 0;
+    // }
 
-    if (player1.y >= window_w - player1.h)
-    {
-        player1.y = window_w - player1.h;
-    }
+    // if (player1.y >= window_w - player1.h)
+    // {
+    //     player1.y = window_w - player1.h;
+    // }
 
-    if (player1.y <= 0)
-    {
-        player1.y = 0;
-    }
+    // if (player1.y <= 0)
+    // {
+    //     player1.y = 0;
+    // }
 
-    ball.y += ball_y_speed * delta_time;
-    if ((ball.y < 0) || (ball.y > (window_h - ball.h)))
-    {
-        ball_y_speed *= -1.05f;
-    }
+    // ball.y += ball_y_speed * delta_time;
+    // if ((ball.y < 0) || (ball.y > (window_h - ball.h)))
+    // {
+    //     ball_y_speed *= -1.05f;
+    // }
 
-    ball.x += ball_x_speed * delta_time;
-    if (ball_x_speed < 0)
-    {
-        if ((ball.y + ball.h >= player1.y) && (ball.y <= player1.y + player1.h))
-        {
-            if (ball.x <= 0 + player1.w)
-            {
-                ball_x_speed *= -1.05f;
-            }
-        }
-        if (ball.x <= 0)
-        {
-            std::cout << "Player 2 Won" << std::endl;
-            isRunning = false;
-        }
-    }
+    // ball.x += ball_x_speed * delta_time;
+    // if (ball_x_speed < 0)
+    // {
+    //     if ((ball.y + ball.h >= player1.y) && (ball.y <= player1.y + player1.h))
+    //     {
+    //         if (ball.x <= 0 + player1.w)
+    //         {
+    //             ball_x_speed *= -1.05f;
+    //         }
+    //     }
+    //     if (ball.x <= 0)
+    //     {
+    //         std::cout << "Player 2 Won" << std::endl;
+    //         isRunning = false;
+    //     }
+    // }
 
-    else if (ball_x_speed > 0)
-    {
-        if ((ball.y + ball.h >= player2.y) && (ball.y <= player2.y + player2.h))
-        {
-            if (ball.x >= (window_w - ball.w - player2.w))
-            {
-                ball_x_speed *= -1.05f;
-            }
-        }
-        if (ball.x >= window_w - ball.w)
-        {
-            std::cout << "Player 1 Won" << std::endl;
-            isRunning = false;
-        }
-    }
+    // else if (ball_x_speed > 0)
+    // {
+    //     if ((ball.y + ball.h >= player2.y) && (ball.y <= player2.y + player2.h))
+    //     {
+    //         if (ball.x >= (window_w - ball.w - player2.w))
+    //         {
+    //             ball_x_speed *= -1.05f;
+    //         }
+    //     }
+    //     if (ball.x >= window_w - ball.w)
+    //     {
+    //         std::cout << "Player 1 Won" << std::endl;
+    //         isRunning = false;
+    //     }
+    // }
 }
 
 void Game::render()
@@ -156,12 +186,21 @@ void Game::render()
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 1);
     // SDL_RenderFillRect(renderer, &ball);
 
-    SDL_Surface *surface = IMG_Load("./src/Ganon_Jumping_Sprite.PNG");
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    // SDL_Surface *surface = IMG_Load("./src/Rei1.0.png");
+    // SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
 
-    SDL_FreeSurface(surface);
-    SDL_RenderCopy(renderer, texture, nullptr, &ball);
-    SDL_DestroyTexture(texture);
+    // SDL_FreeSurface(surface);
+    // SDL_RenderCopy(renderer, texture, nullptr, &ball);
+    // SDL_DestroyTexture(texture);
+
+    // tex->executeShader(fragment);
+    // tex->renderWithShader(100, 100, 500, 500, fragment);
+    tex->render(100, 100, 500, 500);
+    // Uint32 color = tex.getPixel(135, 209);
+
+    // Uint8 red = (color >> 16) & 0xFF;
+    // Uint8 green = (color >> 8) & 0xFF;
+    // Uint8 blue = color & 0xFF;
 
     SDL_RenderPresent(renderer);
 }
---
+++ src/Game/stexture.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+class STexture
+{
+
+public:
+    STexture(SDL_Renderer *gRenderer, SDL_Window *gWindow);
+    ~STexture();
+    void load(std::string path);
+
+    void render(int x, int y, int w, int h, SDL_Rect *clip = nullptr, double angle = 0.0, SDL_Point *center = nullptr, SDL_RendererFlip flip = SDL_FLIP_NONE);
+    void renderWithShader(int x, int y, int w, int h, Uint32 (*func)(Uint32));
+
+    int getWidth();
+    int getHeight();
+
+    // void free();
+
+    int getPitch();
+    int getPixelCount();
+
+    Uint32 *getPixels();
+    Uint32 getPixel(int x, int y);
+
+    void executeShader(Uint32 (*func)(Uint32, STexture *), STexture *skin);
+
+    void lockTexture();
+    void unLockTexture();
+
+private:
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+
+    int tWidth;
+    int tHeight;
+
+    Uint32 format;
+    SDL_PixelFormat *mappingFormat;
+
+    void *mPixels;
+    int mPitch;
+
+    SDL_Texture *mTexture;
+};
+
+STexture::STexture(SDL_Renderer *gRenderer, SDL_Window *gWindow)
+{
+    renderer = gRenderer;
+    window = gWindow;
+
+    tHeight = 0;
+    tWidth = 0;
+    mPitch = 0;
+
+    format = SDL_GetWindowPixelFormat(window);
+    mappingFormat = SDL_AllocFormat(format);
+}
+
+STexture::~STexture()
+{
+    SDL_FreeFormat(mappingFormat);
+    SDL_DestroyTexture(mTexture);
+    mPixels = nullptr;
+}
+
+void STexture::load(std::string path)
+{
+    SDL_Surface *loadedSurface = IMG_Load(path.c_str());
+
+    SDL_Surface *formattedSurface = SDL_ConvertSurfaceFormat(loadedSurface, format, 0);
+
+    mTexture = SDL_CreateTexture(renderer, format, SDL_TEXTUREACCESS_STREAMING, loadedSurface->w, loadedSurface->h);
+
+    tWidth = formattedSurface->w;
+    tHeight = formattedSurface->h;
+
+    lockTexture();
+
+    memcpy(mPixels, formattedSurface->pixels, formattedSurface->pitch * formattedSurface->h);
+
+    unLockTexture();
+
+    SDL_FreeSurface(loadedSurface);
+    SDL_FreeSurface(formattedSurface);
+}
+
+int STexture::getPitch()
+{
+    return mPitch;
+}
+
+int STexture::getWidth()
+{
+    return tWidth;
+}
+
+int STexture::getHeight()
+{
+    return tHeight;
+}
+
+void STexture::render(int x, int y, int w, int h, SDL_Rect *clip, double angle, SDL_Point *center, SDL_RendererFlip flip)
+{
+    SDL_Rect renderQuad = {x, y, w, h};
+
+    SDL_RenderCopyEx(renderer, mTexture, clip, &renderQuad, angle, center, flip);
+}
+
+void STexture::renderWithShader(int x, int y, int w, int h, Uint32 (*func)(Uint32))
+{
+    SDL_Rect renderQuad = {x, y, w, h};
+
+    lockTexture();
+
+    Uint32 *pixels = getPixels();
+    SDL_Texture *copyTexture = SDL_CreateTexture(renderer, format, SDL_TEXTUREACCESS_STREAMING, tWidth, tHeight);
+    void *copyPixels;
+    int copyPitch;
+    SDL_LockTexture(copyTexture, NULL, &copyPixels, &copyPitch);
+    Uint32 *copyPixelsInt = (Uint32 *)copyPixels;
+
+    for (int i = 0; i < getPixelCount(); i++)
+    {
+        copyPixelsInt[i] = func(pixels[i]);
+    }
+
+    SDL_RenderCopy(renderer, copyTexture, nullptr, &renderQuad);
+
+    SDL_UnlockTexture(copyTexture);
+    SDL_DestroyTexture(copyTexture);
+    unLockTexture();
+}
+
+void STexture::lockTexture()
+{
+    SDL_LockTexture(mTexture, nullptr, &mPixels, &mPitch);
+}
+void STexture::unLockTexture()
+{
+    SDL_UnlockTexture(mTexture);
+    mPixels = nullptr;
+    mPitch = 0;
+}
+
+int STexture::getPixelCount()
+{
+    return tWidth * tHeight;
+}
+
+Uint32 *STexture::getPixels()
+{
+    return (Uint32 *)mPixels;
+}
+
+Uint32 STexture::getPixel(int x, int y)
+{
+    lockTexture();
+    Uint32 *pixels = getPixels();
+    Uint32 pixel = pixels[y * tWidth + x];
+    unLockTexture();
+    return pixel;
+}
+
+void STexture::executeShader(Uint32 (*func)(Uint32, STexture *), STexture *skin)
+{
+    lockTexture();
+    Uint32 *pixels = getPixels();
+
+    for (int i = 0; i < getPixelCount(); i++)
+    {
+        pixels[i] = func(pixels[i], skin);
+    }
+
+    unLockTexture();
+}
+
+// void free()
+// {
+//     SDL_DestroyTexture(mTexture);
+// }
